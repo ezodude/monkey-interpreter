7d6e7a410079bd33bbee86aa3df38e2a69232e38
Ezo Saleh <ezo@the-experimenters.com>
2017-01-17 13:26:00 +0000

    Monkey understands identifiers and digits.

--- token/token.go
+++ token/token.go
@@ -11,34 +11,46 @@ package token
 
 type TokenType string
 
-type Token struct{
-  Type    TokenType
-  Literal string
+type Token struct {
+	Type    TokenType
+	Literal string
 }
 
 const (
-  ILLEGAL = "ILLEGAL"
-  EOF     = "EOF"
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
 
-  //identifiers and literals
-  IDENT   = "IDENT"
-  INT     = "INT"
+	//identifiers and literals
+	IDENT = "IDENT"
+	INT   = "INT"
 
-  //Operators
-  ASSIGN  = "="
-  PLUS    = "+"
+	//Operators
+	ASSIGN = "="
+	PLUS   = "+"
 
-  //Delimiters
-  COMMA       = ","
-  SEMICOLON   = ";"
+	//Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
 
-  LPAREN      = "("
-  RPAREN      = ")"
+	LPAREN = "("
+	RPAREN = ")"
 
-  LBRACE      = "{"
-  RBRACE      = "}"
+	LBRACE = "{"
+	RBRACE = "}"
 
-  //Keywords
-  FUNCTION    = "FUNCTION"
-  LET         = "LET"
+	//Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
